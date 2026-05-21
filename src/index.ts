d99e86e88edcf9ff60aedd77ed4bb6eb95dcc4dc
Amer <38424417+ameralhaj@users.noreply.github.com>
2026-05-21 22:48:02 +0200

    Add Amercharts free package files

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+export * from "./helpers/AmerChartTheme";
+export * from "./helpers/ClientTooltip";
+
+export * from "./area-charts/1_AreaChart";
+export * from "./area-charts/2_AreaChartFull";
+
+export * from "./bar-charts/1_BarChartHorizontal_DIV";
+export * from "./bar-charts/2_BarChartHorizontalLogo_DIV";
+export * from "./bar-charts/3_BarChartGradient_DIV";
+export * from "./bar-charts/4_BarChartBreakdown";
+export * from "./bar-charts/5_BarChartThinBreakdown";
+
+export * from "./line-charts/1_LineChart";
+export * from "./line-charts/2_LineChartCurved";
+export * from "./line-charts/3_LineChartMultiple";
+
+export * from "./other-charts/6_HeatmapChart";
+export * from "./other-charts/8_CandlestickChart";
+
+export * from "./pie-charts/1_PieChart";
+export * from "./pie-charts/4_DonutChart";
+
+export * from "./scatter-charts/1_ScatterChart";
